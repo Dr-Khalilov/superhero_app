326cb3aba20bf5a762b9c8073438fa3aef7f426a
Umar Khalilov <ERMASTER100@gmail.com>
2021-05-01 15:27:25 +0300

    Add superhero model

---
+++ server/models/superhero.js
@@ -0,0 +1,55 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Superhero extends Model {
+    static associate (models) {}
+  }
+  Superhero.init(
+    {
+      nickName: {
+        field: 'nick_name',
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      realName: {
+        field: 'real_name',
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      originDescription: {
+        field: 'origin_description',
+        allowNull: false,
+        type: DataTypes.TEXT,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      catchPhrase: {
+        field: 'catch_phrase',
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Superhero',
+      tableName: 'superheroes',
+      underscored: true,
+    }
+  );
+  return Superhero;
+};
