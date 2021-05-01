ede1be566fe2661a428c5ac09b51a588e87e47ed
Umar Khalilov <ERMASTER100@gmail.com>
2021-05-01 22:27:55 +0300

    Add refactors app and server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('./routers');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', router);
+app.use(require('./middlewares/error.handlers'));
 
 module.exports = app;
