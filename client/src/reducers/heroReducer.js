1f6131789b1c84cb245e3bcdf153f5fbbb7ffa09
Umar Khalilov <ERMASTER100@gmail.com>
2021-05-03 01:26:13 +0300

    Add immer lib

--- client/src/reducers/heroReducer.js
+++ client/src/reducers/heroReducer.js
@@ -6,6 +6,12 @@ const initialState = {
   error: null,
 };
 
+const handlers = {
+  []:()=>{
+    
+  }
+}
+
 function heroReducer (state = initialState, action) {
   switch (action.type) {
     case ACTION_TYPES.GET_HERO_REQUEST: {
@@ -25,6 +31,84 @@ function heroReducer (state = initialState, action) {
       };
     }
     case ACTION_TYPES.GET_HERO_ERROR: {
+      const {
+        payload: { error },
+      } = action;
+      return {
+        ...state,
+        isFetching: false,
+        error,
+      };
+    }
+    case ACTION_TYPES.CREATE_HERO_REQUEST: {
+      return {
+        ...state,
+        isFetching: true,
+      };
+    }
+    case ACTION_TYPES.CREATE_HERO_SUCCESS: {
+      const {
+        payload: { hero },
+      } = action;
+      return {
+        ...state,
+        isFetching: false,
+        heroes: [...state.hero, ...hero],
+      };
+    }
+    case ACTION_TYPES.CREATE_HERO_ERROR: {
+      const {
+        payload: { error },
+      } = action;
+      return {
+        ...state,
+        isFetching: false,
+        error,
+      };
+    }
+    case ACTION_TYPES.UPDATE_HERO_REQUEST: {
+      return {
+        ...state,
+        isFetching: true,
+      };
+    }
+    case ACTION_TYPES.UPDATE_HERO_SUCCESS: {
+      const {
+        payload: { heroes },
+      } = action;
+      return {
+        ...state,
+        isFetching: false,
+        heroes: [...state.heroes, ...heroes],
+      };
+    }
+    case ACTION_TYPES.UPDATE_HERO_ERROR: {
+      const {
+        payload: { error },
+      } = action;
+      return {
+        ...state,
+        isFetching: false,
+        error,
+      };
+    }
+    case ACTION_TYPES.DELETE_HERO_REQUEST: {
+      return {
+        ...state,
+        isFetching: true,
+      };
+    }
+    case ACTION_TYPES.DELETE_HERO_SUCCESS: {
+      const {
+        payload: { heroes },
+      } = action;
+      return {
+        ...state,
+        isFetching: false,
+        heroes: [...state.heroes, ...heroes],
+      };
+    }
+    case ACTION_TYPES.DELETE_HERO_ERROR: {
       const {
         payload: { error },
       } = action;
