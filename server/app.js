652f9f9560eb976f59cec8eae8cbe4e34598b456
Umar Khalilov <ERMASTER100@gmail.com>
2021-05-03 17:03:37 +0300

    Fix .sequelizerc

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ const router = require('./routers');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 app.use(express.static('public'));
+app.use(express.json());
 app.use('/api', router);
 app.use(require('./middlewares/error.handlers'));
 
